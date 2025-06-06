d92fae5fdba7a1ff79fa53a773b3412609452179
ASH <singhashirwad2003@gmail.com>
2025-06-07 05:03:30 +0530

    updated

--- app.js
+++ app.js
@@ -88,5 +88,7 @@ io.on("connection", (uniqsocket) => {
 });
 
 server.listen(3000, ()=>{
-   console.log("Server is running on port 3000");
+  //  console.log("Server is running on port 3000");
 });
+
+module.exports = server;
